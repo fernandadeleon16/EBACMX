8dac8aa16cff9c085674b59d63eff7f4caece211
fernandadeleon16 <fernanda.leongr@gmail.com>
2024-08-20 12:39:16 -0700

    se agrego variable g

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -1,7 +1,10 @@
 #se añadio la variable "c"
+#Añado la varibable f como factor de tendencia 
 a = 10
 b = 30
 c = 30 * 10 * 5
+f = 5
+c = 30 * 100 * 5 * (1 + f /100
 print ('Resultado = ', c)
 
 #Cambiando el factor 102 según requerimientos del negocio 
